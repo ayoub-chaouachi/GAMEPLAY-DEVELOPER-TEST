d292bf8d8a99bbb3c1b855d8b04ebcfec8dee492
ayoub-chaouachi <ayoubch95@gmail.com>
2021-11-24 18:19:48 +0100

    test

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+// This is for testing purposes
+public class Rotator : MonoBehaviour {
+    private void Start() {
+        
+    }
+    private void Update() {
+        
+    }
+}
